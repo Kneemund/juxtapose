cdd5d776c08c33009857cf12ab9c3f78369a32f2
Kneemund <niemand@ymir-graphics.com>
2023-11-12 17:03:13 +0100

    initial canvas implementation

--- static/main.js
+++ static/main.js
@@ -1,12 +1,3 @@
-const beforeImage = document.querySelector('.image-comparison .before-image');
-const afterImage = document.querySelector('.image-comparison .after-image');
-
-const beforeLabel = document.querySelector('.image-comparison .before-label');
-const afterLabel = document.querySelector('.image-comparison .after-label');
-
-const beforeLabelText = beforeLabel.firstElementChild;
-const afterLabelText = afterLabel.firstElementChild;
-
 const urlSearchParams = new URLSearchParams(window.location.search);
 const parameters = {
     data: urlSearchParams.get('d'),
@@ -48,39 +39,172 @@ fetch(`https://api.kneemund.de/juxtapose/url?d=${parameters.data}&m=${parameters
         return data.json();
     })
     .then((data) => {
-        beforeImage.src = data.left_image_url;
-        afterImage.src = data.right_image_url;
-
-        if (data.left_image_label) {
-            beforeLabelText.textContent = data.left_image_label;
-            beforeLabel.style.display = 'flex';
-        }
-
-        if (data.right_image_label) {
-            afterLabelText.textContent = data.right_image_label;
-            afterLabel.style.display = 'flex';
-        }
+        // beforeImage.src = data.left_image_url;
+        // afterImage.src = data.right_image_url;
+        // if (data.left_image_label) {
+        //     beforeLabelText.textContent = data.left_image_label;
+        //     beforeLabel.style.display = 'flex';
+        // }
+        // if (data.right_image_label) {
+        //     afterLabelText.textContent = data.right_image_label;
+        //     afterLabel.style.display = 'flex';
+        // }
     })
     .catch(() => {
-        displayErrorMessage('The API request failed.');
+        // displayErrorMessage('The API request failed.');
     });
 
-const slider = document.querySelector('.image-comparison .slider');
-const sliderLine = document.querySelector('.image-comparison .slider-line');
-const sliderIcon = document.querySelector('.image-comparison .slider-icon');
+function initializeFullscreenCanvas(canvas) {
+    // Get the DPR and size of the canvas
+    const dpr = window.devicePixelRatio;
 
-slider.addEventListener('input', (e) => {
-    const sliderValue = e.target.value + '%';
-    const inverseSliderValue = 100 - e.target.value + '%';
+    // Set the "actual" size of the canvas
+    canvas.width = window.innerWidth * dpr;
+    canvas.height = window.innerHeight * dpr;
 
-    beforeLabel.style['max-width'] = sliderValue;
-    afterLabel.style['max-width'] = inverseSliderValue;
+    // Scale the context to ensure correct drawing operations
+    const ctx = canvas.getContext('2d');
+    ctx.scale(dpr, dpr);
 
-    beforeImage.style.width = sliderValue;
-    sliderLine.style.left = sliderValue;
-    sliderIcon.style.left = sliderValue;
-});
+    // Set the "drawn" size of the canvas
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
 
-slider.addEventListener('mouseup', (e) => {
-    e.target.blur();
-});
+    ctx.fillStyle = 'white';
+    return ctx;
+}
+
+let mousedownIntervalID = -1;
+
+const canvas = document.getElementById('canvas');
+let ctx;
+
+const leftImage = new Image();
+const rightImage = new Image();
+let previewImageWidth = 0;
+let previewImageHeight = 0;
+
+let leftImageWithLabel, rightImageWithLabel;
+
+function draw(offset) {
+    if (parameters.orientation === 'h') {
+        // horizontal
+        ctx.drawImage(leftImageWithLabel, 0, 0, previewImageWidth, offset, 0, 0, previewImageWidth, offset);
+
+        ctx.drawImage(
+            rightImageWithLabel,
+            0,
+            offset,
+            previewImageWidth,
+            previewImageHeight - offset,
+            0,
+            offset,
+            previewImageWidth,
+            previewImageHeight - offset
+        );
+
+        ctx.fillRect(0, offset - 1, previewImageWidth, 2);
+    } else {
+        // vertical
+        ctx.drawImage(leftImageWithLabel, 0, 0, offset, previewImageHeight, 0, 0, offset, previewImageHeight);
+
+        ctx.drawImage(
+            rightImageWithLabel,
+            offset,
+            0,
+            previewImageWidth - offset,
+            previewImageHeight,
+            offset,
+            0,
+            previewImageWidth - offset,
+            previewImageHeight
+        );
+
+        ctx.fillRect(offset - 1, 0, 2, previewImageHeight);
+    }
+}
+
+function updateCanvas(e) {
+    e.stopPropagation();
+    draw(parameters.orientation === 'h' ? e.offsetY : e.offsetX);
+}
+
+function drawText(ctx, alignment, text) {
+    const MARGIN = 14;
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+    ctx.textAlign = 'left';
+    ctx.font = 'bold 28px sans-serif';
+    ctx.textBaseline = 'top';
+
+    const textSize = ctx.measureText(text);
+    console.log(textSize.ideographicBaseline, textSize);
+    const textHeight = textSize.actualBoundingBoxAscent - textSize.ideographicBaseline;
+
+    let x, y;
+    if (alignment === 'top_left') {
+        x = 0;
+        y = 0;
+    } else if (alignment === 'bottom_left') {
+        x = 0;
+        y = previewImageHeight - textHeight - 2 * MARGIN;
+    } else if (alignment === 'bottom_right') {
+        x = previewImageWidth - textSize.width - 2 * MARGIN;
+        y = previewImageHeight - textHeight - 2 * MARGIN;
+    } else {
+        throw new Error('Invalid alignment');
+    }
+
+    ctx.fillRect(x, y, textSize.width + MARGIN * 2, textHeight + MARGIN * 2);
+
+    ctx.fillStyle = 'white';
+    ctx.fillText(text, x + MARGIN, y + MARGIN);
+}
+
+async function drawInitialCanvas() {
+    leftImage.src =
+        'https://cdn.discordapp.com/attachments/1165454160976679033/1166717629763948704/Bildschirmfoto_vom_2022-11-17_20-11-31.png';
+
+    rightImage.src =
+        'https://cdn.discordapp.com/attachments/1165454160976679033/1166717630250496071/Bildschirmfoto_vom_2022-11-17_20-15-40.png';
+
+    await Promise.all([leftImage.decode(), rightImage.decode()]);
+
+    // TODO: preserve aspect ratio
+    previewImageWidth = Math.min(Math.min(leftImage.width, rightImage.width), window.innerWidth);
+    previewImageHeight = Math.min(Math.min(leftImage.height, rightImage.height), window.innerHeight);
+
+    canvas.width = previewImageWidth;
+    canvas.height = previewImageHeight;
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'white';
+
+    const leftImageCanvas = new OffscreenCanvas(previewImageWidth, previewImageHeight);
+    const rightImageCanvas = new OffscreenCanvas(previewImageWidth, previewImageHeight);
+
+    const leftImageCanvasCtx = leftImageCanvas.getContext('2d');
+    const rightImageCanvasCtx = rightImageCanvas.getContext('2d');
+
+    leftImageCanvasCtx.drawImage(leftImage, 0, 0, previewImageWidth, previewImageHeight);
+    rightImageCanvasCtx.drawImage(rightImage, 0, 0, previewImageWidth, previewImageHeight);
+
+    drawText(leftImageCanvasCtx, parameters.orientation === 'h' ? 'top_left' : 'bottom_left', 'Left');
+    drawText(rightImageCanvasCtx, parameters.orientation === 'h' ? 'bottom_left' : 'bottom_right', 'Right');
+
+    leftImageWithLabel = leftImageCanvas.transferToImageBitmap();
+    rightImageWithLabel = rightImageCanvas.transferToImageBitmap();
+
+    draw(parameters.orientation === 'h' ? previewImageHeight / 2 : previewImageWidth / 2);
+
+    canvas.addEventListener('mousedown', (e) => {
+        updateCanvas(e);
+        canvas.addEventListener('mousemove', updateCanvas);
+    });
+
+    canvas.addEventListener('mouseup', () => {
+        canvas.removeEventListener('mousemove', updateCanvas);
+    });
+}
+
+drawInitialCanvas();
+// window.onresize = drawInitialCanvas;
